655bb225380a080a3d250d5f3b99dbc3ef3ba120
Tenzin Kalsang <109454940+fawfol@users.noreply.github.com>
2025-07-02 18:24:26 +0530

    Create CustomDrawer.js

---
+++ ForLover/src/screens/en/navigation/CustomDrawer.js
@@ -0,0 +1,30 @@
+// src/screens/en/navigation/CustomDrawer.js
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { DrawerContentScrollView, DrawerItemList } from '@react-navigation/drawer';
+
+export default function CustomDrawer(props) {
+  return (
+    <DrawerContentScrollView {...props} contentContainerStyle={styles.container}>
+      <Text style={styles.caption}>Menu</Text>
+      <DrawerItemList {...props} />
+    </DrawerContentScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 50,
+  },
+  caption: {
+    textAlign : 'left',
+    fontSize: 26,
+    fontWeight: 'bold',
+    color: '#aa336a',
+    paddingHorizontal: 20,
+    paddingBottom: 15,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+    marginBottom: 10,
+  },
+});
