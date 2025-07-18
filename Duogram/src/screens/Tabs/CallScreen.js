ff2fbeb814e1aa193be94d2df3d27466c2f8785f
Tenzin Kalsang <109454940+fawfol@users.noreply.github.com>
2025-07-18 14:09:55 +0530

    Create CallScreen.js

---
+++ Duogram/src/screens/Tabs/CallScreen.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+export default function VideoCallScreen() {
+  return (
+    <View>
+      <Text>Video Call Screen (Video capabilities will go here)</Text>
+    </View>
+  );
+}
